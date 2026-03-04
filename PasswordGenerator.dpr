da2c3deee94b5fc75acad298dd3e3cde9bb6042e
K@S.Y <ferghus@protonmail.ch>
2026-03-04 17:32:37 +0900

    Common 폴더 삭제 - 개별 폴더 대신 상위 경로의 공용 모듈을 참조하도록 변경

--- PasswordGenerator.dpr
+++ PasswordGenerator.dpr
@@ -1,18 +1,18 @@
 ﻿program PasswordGenerator;
 
 uses
-  Vcl.Forms,
   Winapi.Windows,
+  Vcl.Forms,
   uMain in 'uMain.pas' {frmMain},
-  uAppSettings in 'uAppSettings.pas',
+  uAppController in 'uAppController.pas',
   uAppMenu in 'uAppMenu.pas',
+  uAppSettings in 'uAppSettings.pas',
   uPassword in 'uPassword.pas',
   uAppStrings in 'uAppStrings.pas',
-  uForms in 'Common\uForms.pas',
-  uMenu in 'Common\uMenu.pas',
-  uMessageBox in 'Common\uMessageBox.pas',
-  uSettings in 'Common\uSettings.pas',
-  uAppController in 'uAppController.pas';
+  uForms in '..\Common\uForms.pas',
+  uMenu in '..\Common\uMenu.pas',
+  uMessageBox in '..\Common\uMessageBox.pas',
+  uSettings in '..\Common\uSettings.pas';
 
 var
   uMutex: THandle;
